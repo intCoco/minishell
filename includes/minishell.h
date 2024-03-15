762472e71008a15d2840ba57f5afb080ed786d0b
intCoco <corentin.huchard@gmail.com>
2024-03-15 22:34:22 +0100

    Header

---
+++ includes/minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chuchard <chuchard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/15 22:26:45 by chuchard          #+#    #+#             */
+/*   Updated: 2024/03/15 22:28:37 by chuchard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/includes/libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+# define PROMPT "\033[1;34mminishell\033[0m> "
+
+typedef struct
+{
+	int type;    // Type de token
+	char *value; // Valeur du token (le texte réel du token)
+}	t_token;
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,40 +6,30 @@
 /*   By: chuchard <chuchard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 10:55:48 by chuchard          #+#    #+#             */
-/*   Updated: 2024/03/15 22:04:27 by chuchard         ###   ########.fr       */
+/*   Updated: 2024/03/15 22:27:30 by chuchard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <readline/history.h>
-#include <readline/readline.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include "../libft/includes/libft.h"
-
-typedef struct
-{
-	int type;    // Type de token
-	char *value; // Valeur du token (le texte réel du token)
-}		t_token;
+#include "../includes/minishell.h"
 
 void	handle_sig(int sig)
 {
 	if (sig == SIGINT)
-		ft_putstr_fd("\n\033[1;34mminishell\033[0m> ", 1);
+	{
+		ft_putchar_fd('\n', 1);
+		ft_putstr_fd(PROMPT, 1);
+	}
 }
 
 int	main(void)
 {
 	char *input;
-	
+
 	signal(SIGINT, handle_sig);
 	signal(SIGQUIT, handle_sig);
 	while (1)
 	{
-		input = readline("\033[1;34mminishell\033[0m> ");
+		input = readline(PROMPT);
 		if (input == NULL)
 			break ;
 		if (ft_strcmp(input, "") != 0)
